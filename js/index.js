90df0b866e3880493b6fe362b01a65c8ee5e1760
Franco <franco.p.0109@gmail.com>
2020-11-05 18:03:16 -0300

    First commit

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function imgSlider(params) {
+	document.querySelector('.starbucks').src = params;
+}
+
+function changeCircleColor(color) {
+	const circle = document.querySelector('.circle');
+	circle.style.background = color;
+}
